e07617514d61f8df081bd434a7e5c09b3a6c6ffe
bagel <freetgm@gmail.com>
2013-10-08 10:29:21 +0800

    add day

--- wsgi/status/status.py
+++ wsgi/status/status.py
@@ -12,24 +12,7 @@ import shortcut
 import time
 import urlparse
 import redis
-import threading
-import Queue
 
-
-class Multi(threading.Thread):
-    def __init__(self, pipe, keys, n, q):
-        threading.Thread.__init__(self)
-        self.pipe = pipe
-        self.keys = keys
-        self.n = n
-        self.q = q
-
-    def run(self):
-        #[ self.pipe.hget(k, self.n) for k in self.keys ]
-        for k in self.keys:
-            self.pipe.hget(k, self.n)
-        print "work"
-        self.q.put(self.pipe.execute())
 
 class Status:
     def __init__(self, environ, template):
@@ -42,41 +25,6 @@ class Status:
         self.idc = eval(self.r.get("idc"))
         self.mod = eval(self.r.get("mod"))
         self.domain = eval(self.r.get("domain"))
-
-    def sip(self):
-        ips = []
-        if self.query.has_key("ip"):
-            ip = self.query["ip"][0]
-            caption = ip
-            ips.append(ip)
-        else:
-            if self.query.has_key("idc") and not self.query.has_key("mod"):
-                idc = self.query["idc"][0]
-                caption = idc
-                for mod in self.mod.keys():
-                    if self.node[idc].has_key(mod):
-                        ips.extend(self.node[idc][mod])
-            elif not self.query.has_key("idc") and self.query.has_key("mod"):
-                mod = self.query["mod"][0]
-                caption = mod
-                for idc in self.idc.keys():
-                    if self.node[idc].has_key(mod):
-                        ips.extend(self.node[idc][mod])
-            elif not self.query.has_key("idc") and not self.query.has_key("mod"):
-                caption = time.strftime("%Y-%m-%d", time.localtime())
-                for mod in self.mod.keys():
-                    for idc in self.idc.keys():
-                        if self.node[idc].has_key(mod):
-                            ips.extend(self.node[idc][mod])
-            else:
-                idc = self.query["idc"][0]
-                mod = self.query["mod"][0]
-                caption = " ".join([idc, mod])
-                if self.node[idc].has_key(mod):
-                    ips.extend(self.node[idc][mod])
-                else:
-                    ips = ["0.0.0.0"]
-        return (ips, caption)
 
     def idcMod(self):
         idcs = self.idc.keys()
@@ -100,31 +48,33 @@ class Status:
     def data(self):
         t = int(time.time())
         t1 = time.time()
-        if self.query.has_key("time"):
-            if self.query["time"][0] == "30min":
-                offset = 60
-                m = 1            
-                xname = "30 minutes"
-            elif self.query["time"][0] == "hour":
-                offset = 60
-                m = 2
-                xname = "hour"
-            elif self.query["time"][0] == "4hour":
-                offset = 60
-                m = 4
-                xname = "4 hours"
-            elif self.query["time"][0] == "day":
-                offset = 240
-                m = 12
-                xname = "day"
-            elif self.query["time"][0] == "week":
-                offset = 240 * 7
-                m = 12
-                xname = "week"
-        else:
+        qtime = self.query.get("time", ["30min"])[0]
+        if qtime == "30min":
             offset = 60
-            m = 1
+            m = 1            
             xname = "30 minutes"
+        elif qtime == "hour":
+            offset = 60
+            m = 2
+            xname = "hour"
+        elif qtime == "4hour":
+            offset = 60
+            m = 4
+            xname = "4 hours"
+        elif qtime == "day":
+            offset = 240
+            m = 12
+            xname = "day"
+        elif qtime == "week":
+            offset = 240 * 7
+            m = 12
+            xname = "week"
+        else:
+            t = int(time.mktime(time.strptime(qtime, "%Y-%m-%d"))) + 86400
+            offset = 240
+            m = 12
+            xname = qtime
+
         t = t - t % offset - 30 * m * offset
         data = []
         print "1:", time.time() -  t1
@@ -159,9 +109,15 @@ class Status:
                 n = 0
             if j % (3 * m) == 0:
                 data.append({"vline": "true", "lineposition": "1", "color": "dddddd", "thickness": "1.5"})
-                data.append({"label": time.strftime("%H:%M", time.localtime(t)), "value": str(int(n)/60), "tooltext": "%s, %s" % (time.strftime("%H:%M", time.localtime(t)), str(int(n)/60))})
+                if xname == "week":
+                    data.append({"label": time.strftime("%m/%d %H:%M", time.localtime(t)), "value": str(int(n)/60), "tooltext": "%s, %s" % (time.strftime("%m/%d %H:%M", time.localtime(t)), str(int(n)/60))})
+                else:
+                    data.append({"label": time.strftime("%H:%M", time.localtime(t)), "value": str(int(n)/60), "tooltext": "%s, %s" % (time.strftime("%H:%M", time.localtime(t)), str(int(n)/60))})
             else:
-                data.append({"value": str(int(n)/60), "tooltext": "%s, %s" % (time.strftime("%H:%M", time.localtime(t)), str(int(n)/60))})
+                if xname == "week":
+                    data.append({"value": str(int(n)/60), "tooltext": "%s, %s" % (time.strftime("%m/%d %H:%M", time.localtime(t)), str(int(n)/60))})
+                else:
+                    data.append({"value": str(int(n)/60), "tooltext": "%s, %s" % (time.strftime("%H:%M", time.localtime(t)), str(int(n)/60))})
             j += 1
             t += offset
         print "5:", time.time() -  t1
@@ -190,6 +146,10 @@ class Status:
 
     def response(self):
         times = ["30min", "hour", "4hour", "day", "week"]
+        t = time.time()
+        for i in xrange(1, 7):
+            times.append(time.strftime("%Y-%m-%d", time.localtime(t - i * 86400)))
+        
         tdict = {
             "query_string": self.environ["QUERY_STRING"],
             "qdomain": self.query.get("domain", [""])[0],
