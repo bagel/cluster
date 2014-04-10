278d71be11188d903a9baa82d9f61762ae54b793
bagel <freetgm@gmail.com>
2014-04-11 00:52:46 +0800

    profile update

--- app/profile/profile.py
+++ app/profile/profile.py
@@ -92,6 +92,9 @@ class Profile:
         return ("application/json", json.JSONEncoder().encode({"success": 0}))
 
     def domainStat(self, start=0, num=10):
+        """domains that user admin and user has permission, include every domain's users,
+           {"example.com": ["caoyu2", ...], ... }
+        """
         domains = set()
         domain_admin = self.r.hget(self.key_admin, self.user)
         if domain_admin:
@@ -116,6 +119,8 @@ class Profile:
         return (domain_stat, count)
 
     def responseStat(self):
+        """if no query then response count of domains.
+        """
         start = int(self.query.get("start", [0])[0])
         num = int(self.query.get("num", [0])[0])
         if num == 0:
@@ -124,8 +129,10 @@ class Profile:
             return ("application/json", json.JSONEncoder().encode(self.domainStat(start, num)[0]))
 
     def responseStatDomain(self):
+        """check one domain permit users.
+        """
         domain = self.query.get("domain", [""])[0]
-        stat = self.r.hget("info_user_domain", domain)
+        stat = self.r.hget(self.key_domain, domain)
         if stat:
             stat = eval(stat)
             stat = ','.join([ u.split('@')[0] for u in stat ])
