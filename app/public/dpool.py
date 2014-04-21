3d92518e2b26c05141873d17b213f3d4ec756568
bagel <freetgm@gmail.com>
2014-04-22 02:11:30 +0800

    add expire to cachefunc

--- app/public/dpool.py
+++ app/public/dpool.py
@@ -5,11 +5,16 @@ import json
 from util import cachefunc
 
 
-@cachefunc
+@cachefunc(expire=3600)
 def get_serverlist(mod):
     url = 'http://dpadmin.grid.sina.com.cn/api/serverlist.php?mod=' + mod
     return [ s["ip_in"] for s in json.loads(urllib2.urlopen(url=url).read())["list"] ]
 
+@cachefunc(expire=10, miss=1)
+def test():
+    return (1, 2)
+
 
 if __name__ == "__main__":
-    print get_serverlist(mod="varnish")
+    #print get_serverlist(mod="varnish")
+    print test()
--- app/public/util.py
+++ app/public/util.py
@@ -4,20 +4,31 @@ import hashlib
 import redis
 
 
+def cachefunc(expire=600, miss=0):
+    def _cachefunc(f):
+        def __cachefunc(*args, **kwargs):
+            r = redis.StrictRedis("10.13.32.21", "6381")
+            keys = ["info_func-", f.func_name]
+            keys.extend([ '-%s' % arg for arg in args ])
+            keys.extend([ '-%s--%s' % (k, v) for k, v in kwargs.iteritems() ])
+            key = ''.join(keys)
+            print key
+            if not r.exists(key) or miss:
+                print "miss"
+                value = f(*args, **kwargs)
+                r.setex(key, expire, value)
+                return value
+            else:
+                print "hit"
+                value = r.get(key)
+                try:
+                    return eval(value, {}, {})
+                except NameError:
+                    return value
+        return __cachefunc
+    return _cachefunc
+
+
 def userkey(user):
     return hashlib.md5('%s@dpooluser' % user).hexdigest()
 
-def cachefunc(f):
-    def _cachefunc(*args, **kwargs):
-        r = redis.StrictRedis("10.13.32.21", "6381")
-        keys = [f.func_name]
-        keys.extend([ '-%s' % arg for arg in args ])
-        keys.extend([ '-%s--%s' % (k, v) for k, v in kwargs.iteritems() ])
-        key = ''.join(keys)
-        if not r.hexists('info_func', key):
-            value = f(*args, **kwargs)
-            r.hset('info_func', key, value)
-            return value
-        else:
-            return r.hget('info_func', key)
-    return _cachefunc
