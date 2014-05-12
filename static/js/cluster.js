86defff669927c40c5a5658f111d1d9e127a209d
bagel <freetgm@gmail.com>
2014-05-12 21:14:27 +0800

    add page js

--- static/js/cluster.js
+++ static/js/cluster.js
@@ -17,6 +17,67 @@ function dateFormat(t) {
         hour = d.getHours() < 10 ? '0' + d.getHours() : d.getHours(),
         minute = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes(),
         second = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds();
-    return [year, month, day, hour, minute, second]
+    return [year, month, day, hour, minute, second];
+}
+
+function page(s, url, num, id, func) {
+    $.getJSON(url, function(jsonData) {
+        var pagehtml = "";
+        count = jsonData["count"];
+        if ( count % num != 0 ) {
+            var pageMax = Math.floor(count / num + 1);
+        } else {
+            var pageMax = count / num;
+        }
+        if (!s) {
+            s = 1;
+        }
+        if (s == 1) {
+            if ( pageMax != 0 ) {
+                pagehtml += '<li class="disabled"><a>1</a></li>';
+            }
+        } else {
+            pagehtml += '<li><a onclick=' + func + '(' + (s - 1) + ')>&laquo;</a></li>';
+            pagehtml += '<li><a onclick=' + func + '(1)>1</a></li>';
+            if ( s != 2 ) {
+                pagehtml += '<li class="disabled"><a>...</a></li>';
+            }
+        }
+        if ( s != 1 && s != pageMax) {
+            pagehtml += '<li class="disabled"><a>' + s + '</a></li>';
+        }
+        if ( pageMax > 1) {
+            if ( s == pageMax ) {
+                pagehtml += '<li class="disabled"><a>' + pageMax + '</a></li>';
+            } else {
+                if ( s != (pageMax - 1) ) {
+                    pagehtml += '<li class="disabled"><a>...</a></li>';
+                }
+                pagehtml += '<li><a onclick=' + func + '(' + pageMax + ')>' + pageMax + '</a></li>';
+                pagehtml += '<li><a onclick=' + func + '(' + (s + 1) + ')>&raquo;</a></li>';
+            }
+        }
+        document.getElementById(id).innerHTML = pagehtml;
+    });
+}
+
+function pageContent(s, url, num, id, func) {
+    if(!s) {
+        s = 1;
+    }
+    var st = (s - 1) * num;
+    var url = url + "?num=" + num.toString() + "&start=" + st;
+    $.getJSON(url, function(jsonData) {
+        var contenthtml = "";
+        for (i in jsonData) {
+            contenthtml += "<tr>";
+            for (j in jsonData[i]) {
+                contenthtml += "<td>" + jsonData[i][j] + "</td>";
+            }
+            contenthtml += "</tr>";
+        }
+        document.getElementById(id).innerHTML = contenthtml;
+    });
+    func(s);
 }
 
